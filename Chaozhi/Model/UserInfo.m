218246e6b5913f379e8a3dd3a262ae08547e92bd
hanzhanbing <1655661337@qq.com>
2018-11-03 19:52:44 +0800

    我的页面头像显示

--- Chaozhi/Model/UserInfo.m
+++ Chaozhi/Model/UserInfo.m
@@ -44,4 +44,10 @@ static NSUserDefaults *_defaults = nil;
     return [CacheUtil getCacherWithKey:@"token"];
 }
 
+- (void)setHead_img_url:(NSString *)head_img_url {
+    if (_head_img_url != head_img_url) {
+        _head_img_url = [NSString stringWithFormat:@"http:%@",head_img_url];
+    }
+}
+
 @end
